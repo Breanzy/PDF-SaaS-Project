532ab5b04a8df45a156a437beec66c092db8671a
Brean Julius Carbonilla <julius.carbonilla@gmail.com>
2025-04-03 21:00:05 +1100

    Clerk Authentication

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+    return (
+        <main className="">
+            <h1>PDF AI SaaS</h1>
+            button.tsx
+        </main>
+    );
+}
