06cceac7a3eb8fa84d6af8e4aeac576334aaa15a
Brean Julius Carbonilla <julius.carbonilla@gmail.com>
2025-04-22 17:58:14 +1000

    create dashboard page

---
+++ components/Documents.tsx
@@ -0,0 +1,9 @@
+import PlaceholderDocument from "./PlaceholderDocument";
+
+export default function Documents() {
+    return (
+        <div className="flex flex-wrap p-5 bg-gray-100 justify-center lg:justify-star rounded-sm gap-6 max-2-7xl mx-auto">
+            <PlaceholderDocument />
+        </div>
+    );
+}
