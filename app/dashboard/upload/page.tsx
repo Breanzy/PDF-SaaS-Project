bdcde4d8a591a581083d803912dd362310801abe
Brean Julius Carbonilla <julius.carbonilla@gmail.com>
2025-05-10 22:48:06 +1000

    i swear i'll lock in tomorrow frfrfrfrfrfrfrfrfrfrfrf

--- app/dashboard/upload/page.tsx
+++ app/dashboard/upload/page.tsx
@@ -3,7 +3,7 @@ import FileUploader from "@/components/FileUploader";
 export default function UploadPage() {
     return (
         <div>
-            <FileUploader />jhhj
+            <FileUploader />
         </div>
     );
 }
