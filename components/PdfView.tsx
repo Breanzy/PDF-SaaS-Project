7dc0a12cd7806733548b4afd48469e665e56e34c
Brean Julius Carbonilla <julius.carbonilla@gmail.com>
2025-05-19 22:16:08 +1000

    Added PdfView

---
+++ components/PdfView.tsx
@@ -0,0 +1,49 @@
+"use client";
+import "react-pdf/dist/Page/AnnotationLayer.css";
+import "react-pdf/dist/Page/TextLayer.css";
+
+import { Document, Page, pdfjs } from "react-pdf";
+import { useEffect, useState } from "react";
+import { Button } from "./ui/button";
+import { Loader2Icon, RotateCw, ZoomInIcon, ZoomOutIcon } from "lucide-react";
+
+pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`;
+export default function PdfView({ url }: { url: string }) {
+    const [numPages, setNumPages] = useState<number>();
+    const [pageNumber, setPageNumber] = useState<number>(1);
+    const [file, setFile] = useState<Blob | null>(null);
+    const [rotation, setRotation] = useState<number>(0);
+    const [scale, setScale] = useState<number>(1);
+
+    useEffect(() => {
+        const fetchFile = async () => {
+            const response = await fetch(url);
+            const file = await response.blob();
+        };
+    }, [url]);
+
+    const onDocumentLoadSuccess = ({
+        numPages,
+    }: {
+        numPages: number;
+    }): void => {
+        setNumPages(numPages);
+    };
+    return (
+        <div>
+            <Document
+                loading={null}
+                file={file}
+                rotate={rotation}
+                onLoadSuccess={onDocumentLoadSuccess}
+                className="m-4 overflow-scroll"
+            >
+                <Page
+                    className="shadow-lg"
+                    scale={scale}
+                    pageNumber={pageNumber}
+                ></Page>
+            </Document>
+        </div>
+    );
+}
